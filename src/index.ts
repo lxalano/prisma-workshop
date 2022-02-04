6b4e84f4bf771a82a6912e2f59fcb87c06a15f3f
John Doe <johndoe@example.com>
2022-02-04 13:23:53 +0800

    更改

--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,118 @@
-import { PrismaClient } from "@prisma/client";
-import express from "express";
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
-app.get("/users", async (req, res) => {
-    const result = await prisma.user.findMany();
-    res.json(result)
+app.get('/users', async (req, res) => {
+  const result = await prisma.user.findMany();
+  res.json(result);
 });
 
 app.post(`/signup`, async (req, res) => {
-    const { name, email } = req.body;
+  const { name, email } = req.body;
 
-    const result = await prisma.user.create({
-        data: {
-            name,
-            email,
-        }
-    });
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
 
-    res.json(result)
+  res.json(result);
 });
 
 app.post(`/post`, async (req, res) => {
-    const { title, content, authorEmail } = req.body;
+  const { title, content, authorEmail } = req.body;
 
-    const result = await prisma.post.create({
-        data: {
-            title,
-            content,
-            author: {
-                connect: {
-                    email: authorEmail
-                },
-            },
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: {
+        connect: {
+          email: authorEmail,
         },
-    });
+      },
+    },
+  });
 
-    res.json(result)
+  res.json(result);
 });
 
-app.put("/post/:id/views", async (req, res) => {
-    const { id } = req.params;
+app.put('/post/:id/views', async (req, res) => {
+  const { id } = req.params;
 
-    const result = await prisma.post.update({
-        where: {
-            id: Number(id),
-        },
-        data: {
-            viewCount: {
-                increment: 1,
-            },
-        },
-    });
+  const result = await prisma.post.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      viewCount: {
+        increment: 1,
+      },
+    },
+  });
 
-    res.json(result)
+  res.json(result);
 });
 
-app.put("/publish/:id", async (req, res) => {
-    const { id } = req.params;
-    const result = await prisma.post.update({
-        where: {
-            id: Number(id),
-        },
-        data: {
-            published: true,
-        },
-    });
+app.put('/publish/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await prisma.post.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      published: true,
+    },
+  });
 
-    res.json(result)
+  res.json(result);
 });
 
-app.get("/user/:id/drafts", async (req, res) => {
-    const { id } = req.params;
+app.get('/user/:id/drafts', async (req, res) => {
+  const { id } = req.params;
 
-    const result = await prisma.user.findUnique({
-        where: {
-            id: Number(id),
-        },
-    }).posts({
-        where: {
-            published: false,
-        },
+  const result = await prisma.user
+    .findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+    .posts({
+      where: {
+        published: false,
+      },
     });
 
-    res.json(result)
+  res.json(result);
 });
 
 app.get(`/post/:id`, async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
+  const result = await prisma.post.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
 
-    const result = await prisma.post.findUnique({
-        where: {
-            id: Number(id),
-        }
-    });
-
-    res.json(result)
+  res.json(result);
 });
 
-app.get("/feed", async (req, res) => {
-    const { searchString, skip, take } = req.query;
-    const result = await prisma.post.findMany({
-        where: {
-            published: true,
-        },
-    });
+app.get('/feed', async (req, res) => {
+  const { searchString, skip, take } = req.query;
+  const result = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+  });
 
-    res.json(result)
+  res.json(result);
 });
 
 app.listen(3000, () =>
-    console.log(`🚀 Server ready at: http://localhost:3000`)
+  console.log(`🚀 Server ready at: http://localhost:3000`)
 );
